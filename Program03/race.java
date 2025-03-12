c100c8279a5ece28b3ab385f83d3cbf97afcec09
Kris <33873725+retreat896@users.noreply.github.com>
2025-03-12 10:16:29 -0500

    updated race.java

--- Program03/race.java
+++ Program03/race.java
@@ -1,42 +1,42 @@
 
-class Racer implements ______________{ // input the interface name
+class Racer implements Runnable { // input the interface name
 
    private String name;
-   private int M = 0;              // these fields are shared by both
-   private volatile long sum = 0;  // threads since there is one object
+   private int M = 0; // these fields are shared by both
+   private volatile long sum = 0; // threads since there is one object
 
    public Racer(String name, int M) {
       this.name = name;
       this.M = M;
       System.out.println("age()=" + Scheduler.age() + ", "
-         + name + " is alive, M=" + M);
+            + name + " is alive, M=" + M);
    }
 
    private long fn(long j, int k) {
       long total = j;
-      for (int i = 1;  i <= k; i++) total += (2 * i - 1) * (2 * i - 1);
+      for (int i = 1; i <= k; i++)
+         total += (2 * i - 1) * (2 * i - 1);
       return total;
    }
 
-   _____________{  // input the abstrac function name
+   public void run() { // input the abstrac function name
       System.out.println("age()=" + Scheduler.age() + ", "
-         + name + " is running");
-      for (int m = 1; m <= M; m++)
-      {
+            + name + " is running");
+      for (int m = 1; m <= M; m++) {
          /*
           * "N = N + 1" type lost update (race condition) in following line
           */
          sum = fn(sum, m);
       }
       System.out.println("age()=" + Scheduler.age() + ", "
-         + name + " is done, sum = " + sum);
+            + name + " is done, sum = " + sum);
    }
 }
 
 class RaceTwoThreads {
 
    private static int M = 10;
-   private final static int numRacers = ____; // fill in a number that how many threads you want to run
+   private final static int numRacers = 2; // fill in a number that how many threads you want to run
 
    public static void main(String[] args) {
 
@@ -47,38 +47,39 @@ class RaceTwoThreads {
       int ch = -1;
       boolean timeSlicingEnsured = false;
       while ((ch = go.getopt()) != go.optEOF) {
-         if      ((char)ch == 'U') {
-            System.out.println(usage);  System.exit(0);
-         }
-         else if ((char)ch == 'M')
+         if ((char) ch == 'U') {
+            System.out.println(usage);
+            System.exit(0);
+         } else if ((char) ch == 'M')
             M = go.processArg(go.optArgGet(), M);
          else {
-            System.err.println(usage);  System.exit(1);
+            System.err.println(usage);
+            System.exit(1);
          }
       }
       System.out.println("RaceTwoThreads: M=" + M + ", timeSlicingEnsured="
-         + timeSlicingEnsured );
+            + timeSlicingEnsured);
 
       // start the two threads, both in the same object
       // so they share one instance of its variable sum
-      
-	  _________________________// create a thread function object
-      _________________________// create an array of threads
+      Racer r = new Racer("Racer", M);
+      Thread[] threads = new Thread[numRacers];
       for (int i = 0; i < numRacers; i++)
-         __________________ // create a racer thread 
+         threads[i] = new Thread(r, "RacerThread-" + i);
       for (int i = 0; i < numRacers; i++) {
-         __________________ // start each thread
+         threads[i].start();
       }
-      System.out.println("age()=" + Scheduler.age() + 
-                       ", all Racer threads started");
+      System.out.println("age()=" + Scheduler.age() +
+            ", all Racer threads started");
 
       // wait for them to finish if not forced consecutive
       try {
-            for (int i = 0; i < numRacers; i++) 
-				______________ // synchronize threads using join()
-         } catch (InterruptedException e) {
-            System.err.println("interrupted out of join");
+         for (int i = 0; i < numRacers; i++) {
+            threads[i].join(); // synchronize threads using join()
          }
+      } catch (InterruptedException e) {
+         System.err.println("interrupted out of join");
+      }
 
       // correct race-free final value of sum is 2*220 = 440 for M of 10
       // and 2*1335334000 = 2670668000 for M of 2000 (so `long sum' needed)
