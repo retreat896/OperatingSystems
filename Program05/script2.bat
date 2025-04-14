28533f61300e412608f11b92a8414debfe8f3fb4
KAdams <retreat896@gmail.com>
2025-04-13 19:42:11 -0500

    updated program05.docx rnamed script

--- Program05/script2.bat
+++ Program05/script2.bat
@@ -1,10 +1,5 @@
-REM clean up class files
-del *.class
+javac *
 
-REM build the Race.class file
-javac race.java
-
-REM run the Race.class file with many arguments
 java RaceManyThreads -M 10 -N 2
 java RaceManyThreads -M 10 -N 3
 java RaceManyThreads -M 20 -N 4
